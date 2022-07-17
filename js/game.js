bcaf97222457db7bb1f11f7a077fb64496c52ace
Evannfries <evann.fries@yahoo.com>
2022-07-17 13:30:04 -0500

    created words and answers for game

--- js/game.js
+++ js/game.js
@@ -10,3 +10,108 @@ class Player2{
     }
     //inherit class Player1 and change name value
 }
+
+
+const words = [
+    {
+        word: "obnca",
+        correctAnswer: "bacon"
+    },
+    {
+        word: "palep",
+        correctAnswer: "apple"
+    },
+    {
+        word: "rownc",
+        correctAnswer: "crown"
+    },
+    {
+        word: "dorang",
+        correctAnswer: "dragon"
+    },
+    {
+        word: "telapehn",
+        correctAnswer: "elephant"
+    },
+    {
+        word: "wololf",
+        correctAnswer: "follow"
+    },
+    {
+        word: "eargnel",
+        correctAnswer: "general"
+    },
+    {
+        word: "inalhiner",
+        correctAnswer: "hairline"
+    },
+    {
+        word: "nastint",
+        correctAnswer: "instant"
+    },
+    {
+        word: "icetusj",
+        correctAnswer: "justice"
+    },
+    {
+        word: "idgnokm",
+        correctAnswer: "kingdom"
+    },
+    {
+        word: "onesls",
+        correctAnswer: "lesson"
+    },
+    {
+        word: "egenltc",
+        correctAnswer: "neglect"
+    },
+    {
+        word: "geonar",
+        correctAnswer: "orange"
+    },
+    {
+        word: "apetcharu",
+        correctAnswer: "parachute"
+    },
+    {
+        word: "rizedlae",
+        correctAnswer: "realized"
+    },
+    {
+        word: "implse",
+        correctAnswer: "simple"
+    },
+    {
+        word: "hertegot",
+        correctAnswer: "together"
+    },
+    {
+        word: "sluvarine",
+        correctAnswer: "universal"
+    },
+    {
+        word: "daveynir",
+        correctAnswer: "vineyard"
+    },
+    {
+        word: "arorwir",
+        correctAnswer: "warrior"
+    },
+    {
+        word: "ginlouygn",
+        correctAnswer: "youngling"
+    },
+    {
+        word: "lelfpuh",
+        correctAnswer: "helpful"
+    },
+    {
+        word: "loidbel",
+        correctAnswer: "boiled"
+    },
+    {
+        word: "anchomelly",
+        correctAnswer: "melancholly"
+    }
+
+]
